7e7f28df7b321b77a558f726c39497ddce9e0ff1
Borys Movchan <borysmn@axis.com>
2021-12-02 20:56:20 +0100

    2021: day2 solution
    Change-Id: I6d9dab5b67c1c1f6ac323b02b51f7340f5aa55ea

---
+++ 2021/day2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+extern "C" {
+#include "day2cc.h"
+    int yylex();
+    void yyerror(const char *msg);
+
+    void print_part_one(unsigned depth, unsigned distance);
+    void print_part_two(unsigned depth, unsigned distance, unsigned aim);
+}
+
+static std::ifstream f("day2.input");
+
+void yyerror(const char *msg) {
+    std::cout << "Parse error: " << msg << std::endl;
+    exit(1);
+}
+
+int yylex() {
+    std::string word;
+
+    f >> word;
+    if (word.empty()) {
+        return -1;
+    }
+
+    if (word == "up") {
+        return UP;
+    } else if (word == "down") {
+        return DOWN;
+    } else if (word == "forward") {
+        return FORWARD;
+    } else if(std::find_if(word.begin(), word.end(), [](char c) { return isdigit(c);}) != word.end()) {
+        yylval.steps = std::stoul(word);
+        return STEP;
+    } else {
+        return -1;
+    }
+}
+
+void print_part_one(unsigned int depth, unsigned int distance) {
+    std::cout << "P1: depth: " << depth << ", distance: " << distance << std::endl;
+    std::cout << "P1: result: " << depth * distance << std::endl;
+}
+
+void print_part_two(unsigned int depth, unsigned int distance, unsigned int aim) {
+    std::cout << "P2: depth: " << depth << ", distance: " << distance << ", aim: " << aim << std::endl;
+    std::cout << "P2: result: " << depth * distance << std::endl;
+
+}
+
+int main(int argc, char *argv[]) {
+    if (!f.is_open()) {
+        std::cout << "File not open" << std::endl;
+        return 1;
+    }
+
+    return yyparse();
+}
---
+++ 2021/day2cc.y
@@ -0,0 +1,39 @@
+%{
+int yylex();
+void yyerror(const char *msg);
+
+void print_part_one(unsigned depth, unsigned distance);
+void print_part_two(unsigned depth, unsigned distance, unsigned aim);
+
+static unsigned depth;
+static unsigned distance;
+
+static unsigned aim;
+static unsigned depth_p2;
+%}
+
+
+%union {
+    unsigned int steps;
+}
+
+%token <steps> STEP
+%token UP
+%token DOWN
+%token FORWARD
+
+%%
+done: seq { print_part_one(depth, distance);
+            print_part_two(depth_p2, distance, aim);
+          }
+    ;
+
+seq: %empty
+   | seq command
+   ;
+
+command: UP STEP         { depth -= $2; aim -= $2; }
+      | DOWN STEP       { depth += $2; aim += $2; }
+      | FORWARD STEP    { distance += $2; depth_p2 += aim * $2; }
+      ;
+%%
